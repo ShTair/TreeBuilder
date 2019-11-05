10e0635fa850fd58b9489e8dcc729ba9f395e258
ShTair <git@vsh.jp>
2019-11-05 10:14:53 +0900

    インタフェースを実装

---
+++ TreeBuilder/ITreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ShComp.Construction.Tree
+{
+    public interface ITreeNode<T> where T : ITreeNode<T>
+    {
+        int Left { get; set; }
+
+        int Right { get; set; }
+
+        T Parent { set; }
+
+        IList<T> Children { get; }
+    }
+}
